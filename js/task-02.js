bdeacd654acfc150ee03591a680b6de5a302a7c4
Ігор <igor.melnyk2011@gmail.com>
2022-06-27 23:24:58 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsLiEl = ingredients.map(ingredient => {
+  const newIngredient = document.createElement('li');
+  newIngredient.textContent = ingredient;
+  newIngredient.classList.add('item');
+  // console.log(newIngredient);
+  return newIngredient;
+  });
+
+  const ingredientsList = document.querySelector('#ingredients');
+  ingredientsList.append(...ingredientsLiEl);
+  console.log(ingredientsList);  
