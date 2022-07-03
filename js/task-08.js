298a596e1e9a5c8c52bb2e3324b2a993b201bf5e
Ігор <igor.melnyk2011@gmail.com>
2022-07-03 10:58:48 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', onButtonLoginFormSubmit);
 
+function onButtonLoginFormSubmit(event) {
+    event.preventDefault();
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    if (email === '' || password === ''){
+        alert("Будь ласка заповніть всі поля авторизації");
+    }
+    else{
+        const formData = {
+          email,
+          password,
+        };
+        console.log(formData);
+      }
+    return loginForm.reset();
+};
