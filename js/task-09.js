5ca67a8fa7d38af4c2c1358bd39d94c400aca6a0
Ігор <igor.melnyk2011@gmail.com>
2022-07-03 11:21:55 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const spanColorEl = document.querySelector('span.color');
+const changeColorBtn = document.querySelector('button.change-color');
+
+changeColorBtn.addEventListener('click', onColorChangeByClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onColorChangeByClick(){
+  spanColorEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
 }
